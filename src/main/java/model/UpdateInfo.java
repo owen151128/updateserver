f2d036c62fb30720df27d1876ce232ad99a2cda5
owen151128 <owen151128@gmail.com>
2018-11-13 16:01:40 +0900

    UpdateInfo 클래스 추가
    파일의 정보를 가지고 있는 클래스
    Tree 구조의 하나에 Node
    파일의 대한 파일명, 경로, Hash 값, 디렉토리 여부를 가지고 있다.
    디렉토리 일 경우 자식 리스트에 Node 가 존재한다.

---
+++ src/main/java/model/UpdateInfo.java
@@ -0,0 +1,96 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * 파일에 대한 업데이트 정보를 가지고 있는 클래스
+ * UpdateInfoTree 에 대한 Node 이다.
+ */
+public class UpdateInfo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String filePath;
+    private String fileName;
+    private String fileHash;
+    private boolean isDirectory;
+    private ArrayList<UpdateInfo> childList;
+
+    /**
+     * 기본 생성자로써 자식 리스트를 초기화 한다.
+     */
+    public UpdateInfo() {
+        childList = new ArrayList<>();
+    }
+
+    /**
+     * 해당 파일의 경로를 반환하는 메소드
+     * @return String 형태의 해당 파일 경로
+     */
+    public String getFilePath() {
+        return filePath;
+    }
+
+    /**
+     * 해당 파일의 경로를 설정하는 메소드
+     * @param filePath String 형태의 해당 파일 경로
+     */
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * 해당 파일의 파일명을 반환하는 메소드
+     * @return String 형태의 해당 파일명
+     */
+    public String getFileName() {
+        return fileName;
+    }
+
+    /**
+     * 해당 파일의 파일명을 설정하는 메소드
+     * @param fileName String 형태의 해당 파일명
+     */
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * 해당 파일의 Hash 값을 반환하는 메소드
+     * @return String 형태의 해당 Hash 값
+     */
+    public String getFileHash() {
+        return fileHash;
+    }
+
+    /**
+     * 해당 파일의 Hash 값을 설정하는 메소드
+     * @param fileHash String 형태의 해당 Hash 값
+     */
+    public void setFileHash(String fileHash) {
+        this.fileHash = fileHash;
+    }
+
+    /**
+     * 해당 클래스가 담고있는 정보에 대해 디렉토리 여부를 반환하는 메소드
+     * @return boolean 형태의 값 디렉토리 일 경우 true, 파일 일 경우 false
+     */
+    public boolean isDirectory() {
+        return isDirectory;
+    }
+
+    /**
+     * 해당 클래스가 담고있는 정보에 대해 디렉토리 여부를 설정하는 메소드
+     * @param directory 디렉토리 일 경우 true 를 설정 하고, 아닐 경우 false 를 설정 한다.
+     */
+    public void setDirectory(boolean directory) {
+        isDirectory = directory;
+    }
+
+    /**
+     * 해당 클래스의 자식 리스트를 얻는 메소드
+     * @return ArrayList 형태의 자식 리스트
+     */
+    public ArrayList<UpdateInfo> getChildList() {
+        return childList;
+    }
+}
