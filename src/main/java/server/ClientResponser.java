5d782a1b0d981c45317dfd4a16d4b30a5bea7d08
owen151128 <owen151128@gmail.com>
2018-11-13 21:09:42 +0900

    Client Responser 추가
    클라이언트 요청에 따라 응답하는 클래스 추가
    
    request 를 받아오는 메소드 추가
    
    UpdateInfoTree 를 생성해서 클라이언트에 보내는 메소드 추가

---
+++ src/main/java/server/ClientResponser.java
@@ -0,0 +1,103 @@
+package server;
+
+import model.UpdateInfoTree;
+import model.UpdateInfoTreeWrapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * 클라이언트 에서 요청이 들어오면 요청에 대해서 처리하고 응답하는 클래스
+ */
+public class ClientResponser {
+    private Socket clientSocket;
+
+    private BufferedReader br = null;
+    private InputStreamReader isr = null;
+    private ObjectOutputStream oos = null;
+
+    /**
+     * 기본 생성자로 클라이언트와 연결된 Socket 을 받아서 클래스를 초기화 한다.
+     *
+     * @param clientSocket Socket 형태의 클라이언트와 연결된 소켓
+     */
+    public ClientResponser(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    /**
+     * 클라이언트 Request 를 받아서 반환하는 메소드
+     *
+     * @return String 형태의 클라이언트 Request
+     */
+    public String getRequest() {
+        String request = null;
+        try {
+            isr = new InputStreamReader(clientSocket.getInputStream());
+            br = new BufferedReader(isr);
+
+            request = br.readLine();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            System.out.println("I/O failed. check stack trace");
+
+        }
+
+        return request;
+    }
+
+    /**
+     * UpdateInfoTree 를 생성해서 클라이언트 로 전송하는 메소드 local_path 로 부터 UpdateInfoTree를 생성해
+     * 클라이언트에 전송 한다.
+     *
+     * @param local_path String 형태의 UpdateInfoTree를 생성할 대상 경로 지정
+     */
+    public void sendUpdateInfoTree(String local_path) {
+
+        try {
+            UpdateInfoTree tree = new UpdateInfoTree();
+            UpdateInfoTreeWrapper.createUpdateInfoTree(local_path.length(), local_path, tree.getChildList());
+
+            oos = new ObjectOutputStream(clientSocket.getOutputStream());
+            oos.writeObject(tree);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            System.out.println("I/O failed. check stack trace");
+
+        } finally {
+
+            try {
+
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+
+                if (isr != null) {
+                    isr.close();
+                }
+
+                if (br != null) {
+                    br.close();
+                }
+
+                if (oos != null) {
+                    oos.close();
+                }
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+                System.out.println("Stream close Failed! check stack trace");
+
+            }
+
+        }
+    }
+}
