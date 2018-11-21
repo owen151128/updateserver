07be919f7c9ff4060369b08cacf945005eac2322
owen151128 <owen151128@gmail.com>
2018-11-21 11:46:49 +0900

    Main 클래스 및 MainConstants 추가
    서버의 Main 이 되는 클래스 - main 함수 존재 및 서버의 Main 기능이 담겨 있음
    MainConstants 클래스 - Main 클래스에서 사용하는 상수들이 모인 집합 으로
    각종 메시지 및 에러 메시지가 담겨 있음

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,258 @@
 package main;
 
+import server.ClientResponser;
+import util.LogUtil;
+import util.SettingFileManager;
+import util.UpdateInfoManager;
+
+import javax.swing.JOptionPane;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+/**
+ * Update Server 의 Main 클래스 이다.
+ * <p>
+ * main 메소드가 존재하는 클래스
+ */
 public class Main {
+
+    private static ServerSocket server;
+    private static Socket client;
+    private static String clientIp;
+    private static LogUtil log;
+
+    private static UpdateInfoManager manager;
+
+    private static Thread updateThread;
+
+    /**
+     * Update Server 의 main 메소드 이다.
+     *
+     * @param args String[] 형태의 서버 시작시 매개변수가 저장되는 변수 이다.
+     */
     public static void main(String[] args) {
-        
+
+        int port;
+
+        if (args.length == 0) {
+
+            JOptionPane.showMessageDialog(null, MainConstants.ERR_NO_PORT_NUMBER);
+
+            return;
+
+        } else {
+
+            try {
+
+                int tmp = Integer.parseInt(args[0]);
+
+                if (tmp < 0 || tmp > 49152) {
+
+                    JOptionPane.showMessageDialog(null, MainConstants.ERR_IMPOSSIBLE_PORT);
+
+                    return;
+                }
+
+            } catch (NumberFormatException e) {
+
+                JOptionPane.showMessageDialog(null, MainConstants.ERR_WRONG_PORT_NUMBER);
+
+                return;
+            }
+
+            port = Integer.parseInt(args[0]);
+
+        }
+
+        log = new LogUtil();
+
+        try {
+
+            server = new ServerSocket(port);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            log.writeLog(MainConstants.ERR_SERVER_START_FAILED);
+
+        }
+
+        SettingFileManager settingFileManager = SettingFileManager.getInstance();
+
+        if (settingFileManager.getLocal_path() != null) {
+
+            manager = new UpdateInfoManager(settingFileManager.getLocal_path());
+
+        }
+
+        updateThread = new Thread(() -> {
+
+            try {
+
+                while (true) {
+
+                    Thread.sleep(5000);
+                    manager.update();
+
+                }
+            } catch (InterruptedException e) {
+
+                log.writeLog(MainConstants.MSG_UPDATE_INFO_MANAGER_STOP);
+
+            }
+        });
+
+        Thread stopThread = new Thread(() -> {
+
+            Socket socket = null;
+
+            try {
+
+                ServerSocket stop;
+                stop = new ServerSocket(8888);
+
+                while (socket == null) {
+
+                    socket = stop.accept();
+
+                }
+
+                stop();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+                log.writeLog(MainConstants.ERR_SERVER_STOP_FAILED);
+
+            }
+
+        });
+
+        stopThread.start();
+
+        updateThread.start();
+
+        log.writeLog(MainConstants.MSG_SERVER_LOADED);
+
+        while (true) {
+
+            try {
+
+                client = server.accept();
+
+                client.setSoTimeout(MainConstants.TIME_OUT);
+                clientIp = client.getInetAddress().toString().substring(1);
+
+            } catch (SocketException e) {
+
+                break;
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+                log.writeLog(MainConstants.ERR_SERVER_IO_FAILED);
+
+                break;
+            }
+
+            log.writeLog(MainConstants.CLIENT_STATUS.CONNECT, clientIp);
+
+            Thread networkThread = new Thread(() -> {
+
+                ClientResponser connector = new ClientResponser(client);
+                String request = connector.getRequest();
+
+                log.writeLog(clientIp, request);
+
+                switch (request) {
+
+                    case MainConstants.RequestProtocol.UPDATE_INFO_TREE:
+
+                        log.writeLog(MainConstants.CLIENT_STATUS.SEND, clientIp);
+
+                        connector.sendUpdateInfoDTO(settingFileManager.getClient_path(), manager.getRoot());
+
+                        log.writeLog(MainConstants.CLIENT_STATUS.SEND_COMPLETE, clientIp);
+
+                        break;
+
+                    case MainConstants.RequestProtocol.REQUEST_DOWNLOAD_DTO:
+
+                        log.writeLog(MainConstants.CLIENT_STATUS.SEND, clientIp);
+
+                        connector.getDownloadRequestAndSendFileResponseDTO();
+
+                        log.writeLog(MainConstants.CLIENT_STATUS.SEND_COMPLETE, clientIp);
+
+                }
+
+
+                try {
+
+                    client.close();
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                    log.writeLog(MainConstants.ERR_SERVER_STREAM_CLOSE_FAILED);
+
+                }
+
+                log.writeLog(MainConstants.CLIENT_STATUS.DISCONNECT, clientIp);
+
+            });
+
+            networkThread.start();
+        }
+    }
+
+    /**
+     * 서버 종료 시에 호출 되는 메소드
+     * 서버 에서 사용하던 자원 들을 정리하는 역할을 한다.
+     */
+    public static void stop() {
+
+        try {
+
+            if (log != null) {
+
+                log.writeLog(MainConstants.MSG_SERVER_STOP);
+
+            }
+
+            if (client != null) {
+
+                client.close();
+
+            }
+
+            if (server != null) {
+
+                server.close();
+
+            }
+
+            if (log != null) {
+
+                log.close();
+
+            }
+
+            if (updateThread != null) {
+
+                updateThread.interrupt();
+
+            }
+
+            System.exit(0);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            log.writeLog(MainConstants.ERR_RESOURCE_CLEAN_FAILED);
+
+        }
     }
 }
--- src/main/java/main/MainConstants.java
+++ src/main/java/main/MainConstants.java
@@ -2,12 +2,41 @@ package main;
 
 /**
  * Main 클래스에서 사용할 상수들을 모아놓은 클래스
+ * <p>
+ * TIME_OUT : 요청에 대한 Time out 을 나타낸다.
+ * ERR_NO_PORT_NUMBER : 서버 시작 시 매개 변수로 포트 번호가 넘어오지 않았을때 출력할 문자열 이다.
+ * ERR_WRONG_PORT_NUMBER : 서버 시작 시 매개 변수로 포트 번호가 이상한 값으로 넘어왔을때 출력할 문자열 이다.
+ * ERR_IMPOSSIBLE_PORT : 서버 시작 시 매개 변수로 1 ~ 49151 포트 번호가 아닌 번호가 넘어왔을때 출력할 문자열 이다.
+ * ERR_SERVER_START_FAILED : 오류로 인해 서버 시작에 실패 했을 경우 출력할 문자열 이다.
+ * ERR_SERVER_STOP_FAILED : 오류로 인해 서버를 정상적으로 종료하지 못했을 경우 출력할 문자열 이다.
+ * ERR_SERVER_IO_FAILED : 오류로 인해 네트워크 I/O 가 정상적으로 이뤄지지 않았을 경우 출력할 문자열 이다.
+ * ERR_SERVER_STREAM_CLOSE_FAILED : 오류로 인해 통신 시 사용한 Stream 들이 close 되지 않았을 경우 출력할 문자열 이다.
+ * ERR_RESOURCE_CLEAN_FAILED : 오류로 인해 서버 종료 시 서버에서 사용하던 리소스 정리가 재대로 되지 않을 경우 출력할 문자열 이다.
+ * <p>
+ * MSG_UPDATE_INFO_MANAGER_STOP : UpdateInfoManager 가 Stop 될 경우 출력할 문자열이다.
+ * MSG_SERVER_LOADED : 서버 로딩이 완료 되었을 때 출력할 문자열이다.
+ * MSG_SERVER_STOP : 서버가 종료되면 출력 될 문자열이다.
  */
 public class MainConstants {
 
+    static final int TIME_OUT = 10000;
+
+    static final String ERR_NO_PORT_NUMBER = "update Server is required port number";
+    static final String ERR_WRONG_PORT_NUMBER = "update Server port number is wrong";
+    static final String ERR_IMPOSSIBLE_PORT = "update Server port number must set 1 ~ 49151";
+    static final String ERR_SERVER_START_FAILED = "Server start failed. check stack trace";
+    static final String ERR_SERVER_STOP_FAILED = "Stop Failed! check the stack trace";
+    static final String ERR_SERVER_IO_FAILED = "Server I/O Failed. check stack trace";
+    static final String ERR_SERVER_STREAM_CLOSE_FAILED = "Stream close failed! check stack trace";
+    static final String ERR_RESOURCE_CLEAN_FAILED = "resource clean failed!";
+
+    static final String MSG_UPDATE_INFO_MANAGER_STOP = "update Info Manager stop";
+    static final String MSG_SERVER_LOADED = "Server loaded.";
+    static final String MSG_SERVER_STOP = "server stop.";
+
     /**
      * 로그를 찍을때 클라이언트 요청에 대한 상태를 나타내는 열거형
-     *
+     * <p>
      * CONNECT : 클라이언트 와 연결이 성공 하였을 때 사용하는 값
      * SEND : 클라이언트 에 데이터를 전송 할 때 사용하는 값
      * SEND_COMPLETE : 클라이언트 에 데이터 전송을 완료 했을 때 사용하는 값
@@ -18,5 +47,17 @@ public class MainConstants {
         SEND,
         SEND_COMPLETE,
         DISCONNECT
+    }
+
+    /**
+     * 클라이언트 와 서버간의 통신 시 어떤 요청인지를 판단할때 사용되는 클래스
+     * UPDATE_INFO_TREE : UpdateInfoTree 를 요청하는 경우
+     * REQUEST_DOWNLOAD_DTO : DownloadRequestDTO 를 서버에서 받고 FileResponseDTO 를 클라이언트에서 받는 요청을 하는 경우
+     */
+    class RequestProtocol {
+
+        static final String UPDATE_INFO_TREE = "updateInfoDTO";
+        static final String REQUEST_DOWNLOAD_DTO = "downloadRequestDTO";
+
     }
 }
